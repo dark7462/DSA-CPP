65bb8243ed93a0cf75f9cd344c47a35410a11e91
dark7462 <anu870906@gmail.com>
2025-02-15 12:01:54 +0530

    update

--- 8-STL.cpp
+++ 8-STL.cpp
@@ -4,7 +4,8 @@ void print_space(){
     cout << "--------------------------------------------------------\n";
 }
 void pairs(){
-    //pairs is a linear data structure that store 2 similar data types can be used in a nested form too
+    //pairs is a linear data structure that store 2  data type variable can be used in a nested form too
+
     // how to declear a pair
     pair<int , int> p = {23,21};
     cout << p.first << " " << p.second << endl;
@@ -112,6 +113,7 @@ void List(){
     list<int> l ;
     //same as vector but here you can do operation from back and front
     // kind of a double linked list
+    //random access of memory is not possible
 
     l.push_back(3);// push a element in the last of the list
     l.emplace_back(4);// push a element in the last of the list but takes less resorce
@@ -132,6 +134,7 @@ void Deque(){
     deque<int> d ;
     //same as vector but here you can do operation from back and forth
     // kind of a double ened queue
+    // it's basically based on dynamic array so random access of elements are possible
 
     d.push_back(3);// push a element in the last of the deque
     d.emplace_back(4);// push a element in the last of the deque but takes less resorce
@@ -142,17 +145,97 @@ void Deque(){
     dq.pop_front(); // delet the first element form the deque
     dq.pop_back(); // delet the last element from the deque
 
-    //to print all the values in the list we can use the same auto loop
-    for(auto val : dq){ // auto or data type then name of the variable then the name of the list of=r the vector
+    //to print all the values in the deque we can use the same auto loop
+    for(auto val : dq){ // auto or data type then name of the variable then the name of the deque
         cout << val << " ";
     }
-    //the funtions like (size, erase, clear, begin, end, rbeign, rend, insert, front, back) are applicable in list as same as in vectors
+    //the funtions like (size, erase, clear, begin, end, rbeign, rend, insert, front, back) are applicable in deque as same as in vectors
+}
+void Stack(){   //works in LIFO  (last in first out)
+    //can be visualised as a container where you can push the elements from the top
+    // and assecc them from top only
+
+    //push and emplace both worksin stack
+    stack<int> s;
+    s.push(1);  //push the element in bottom of the container
+    s.push(2);
+    s.emplace(3);
+
+    while (!s.empty()){
+        cout << s.top() <<" ";  //fetchs the top value of the stack
+        s.pop();    // pop/delet the top element
+    cout << endl;
+    }
+
+
+    s.push(1);
+    s.push(2);
+    s.emplace(3);
+
+    stack<int> s2;
+    s2.swap(s); // now the empty stack s2 will contain the values of s and now s will be empty
+
+    cout << "size of s now : " << s.size() << endl;   // size of the stack 
+    cout << "size of s2 now : " << s2.size() << endl;
+
+
+}
+void Queue(){   // works in FIFO (first in first out)
+    //element are inserted from the back and comes from the first
+    
+    queue<int> q2;
+    q2.push(1);  // elements pushed
+    q2.push(2);
+    q2.emplace(3);
+    q2.emplace(4);
+
+    queue <int> q1;
+    q1.swap(q2); // all values of q2 will be now inside q1 and q2 is empty now
+    cout << "size of q1 : " << q1.size() << endl;// size of the queue
+    cout << "size of q2 : " <<q2.size() << endl;
+
+    while(!q1.empty()){  //check if the queue is empty
+        cout << q1.front() << " ";   //access the first element 
+        q1.pop();//pops/delets the first element
+    }cout << endl;
+}
+void Priorty_queue(){   //same as queue but gives priority to the elements in accesnding or decending order
+
+    priority_queue<int> pq1;
+    pq1.push(3);
+    pq1.push(10);
+    pq1.push(4);
+    pq1.push(5); // auto matically the elements will be in decending order (greatest to smallest)
+
+    while(!pq1.empty()){
+        cout << pq1.top() << " ";
+        pq1.pop();// first it'll pop the largest element then the 2nd largest then the 3rd largest and go'es on...
+    }cout << endl;
+
+    //for revese order / acesnding order 
+    priority_queue<int, vector<int>, greater<int>> pq2;
+    pq2.push(3);    //time complexity of push and pop is O(log n) in priority_queue because it sorts the elemets
+    pq2.push(10);
+    pq2.push(4);
+    pq2.push(5); // auto matically the elements will be in asending order (smallest to greatest)
+
+    while(!pq2.empty()){
+        cout << pq2.top() << " ";
+        //time complexity of top is O(1)
+        pq2.pop();// first it'll pop the smalles element then the 2nd smallest then the 3rd smallest and go'es on...
+        //time complexity of push and pop is O(log n) in priority_queue because it sorts the elemets
+    }cout << endl;
+
+
 }
 int main(){
     // pairs();
     // print_space();
     // vectors();
-    List();
-    Deque();
+    // List();
+    // Deque();
+    // Stack();
+    // Queue();
+    Priorty_queue();
     return 0;
 }
