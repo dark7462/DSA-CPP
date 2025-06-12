535f1813a04796f23960fd3912ca3351c13777d7
dark7462 <anu870906@gmail.com>
2025-06-12 11:31:20 +0530

    update

---
+++ 3-data-sturcts/7-CircularLinkedList.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node{
+	int data;
+	struct node* next;
+}node;
+
+node *head = NULL, *tail = NULL;
+
+void push(int val){
+	node *newnode = new node;
+	newnode->data = val;
+	newnode->next = NULL;
+	if(head == NULL){
+		head = newnode;
+		tail = newnode;
+	}else{
+		tail->next = newnode;
+		tail = newnode;
+		tail->next = head;
+	}
+}
+void Display(node *head){
+	if (head == NULL) return;
+	node *temp = head;
+	do{
+		cout << temp->data << " ";
+		temp = temp->next;
+	} while(temp != head);
+}
+void del_head() {
+	head = head->next;
+	tail->next = head;
+}
+
+void Delete(int pos) {
+	if (head == NULL) return;
+	if (pos == 0) {
+		del_head();
+		return;
+	}
+	node *curr = head;
+	int count = 0;
+	while (count < pos - 1 && curr->next != head) {
+		curr = curr->next;
+		count++;
+	}
+	if (curr->next == head) {
+		cout << "Invalid Position" << endl;
+		return;
+	}
+	node *temp = curr->next;
+	curr->next = temp->next;
+	if (temp == tail) {
+		tail = curr;
+	}
+}
+
+int main(){
+	push(10);
+	push(20);
+	push(30);
+	Display(head);
+	printf("\n");
+	Delete(1);
+	Display(head);
+	printf("\n");
+	return 0;
+}
